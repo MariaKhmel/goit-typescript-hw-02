a0783c185544faaa68dd406b4923d40a6518ba61
Maria <mariakhmel8@gmail.com>
2024-05-17 01:40:43 +0300

    fix

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -5,14 +5,14 @@ const Loader = (): ReactElement => {
   return (
     <RotatingLines
       visible={true}
-      height="96"
+      // height={96}
       width="96"
-      color="grey"
+      // color="grey"
       strokeWidth="5"
       animationDuration="0.75"
       ariaLabel="rotating-lines-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
+      // wrapperStyle={{}}
+      // wrapperClass=""
     />
   );
 };
