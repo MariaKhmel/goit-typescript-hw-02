f336ada88f462ae220bddeb87c8620d5e5b6f3bf
Maria <mariakhmel8@gmail.com>
2024-05-17 01:34:32 +0300

    fixed

--- src/components/types.ts
+++ src/components/types.ts
@@ -3,15 +3,3 @@ export interface VoidFunction {
   (): void;
 }
 export type HandleSearchBarSubmitFunction = (value: string) => void;
-
-// type RotatingLinesTypes = {
-//   visible: boolean;
-//   height: string;
-//   width: string;
-//   color: string;
-//   strokeWidth: string;
-//   animationDuration: string;
-//   ariaLabel: string;
-//   wrapperStyle: {};
-//   wrapperClass: string;
-// };
